6608fe95ca41ae12b8a6c3fc316686bde54c970a
Loyd Ngei <loydngei@gmail.com>
2024-03-18 17:37:23 +0300

    Sort Arrays using Numpy

---
+++ Numpy/sort.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# np.sort()
+
+np1 = np.array([6,7,3,1,0,2,4,8,9])
+
+# print(np1)
+# print(np.sort(np1))
+
+# Alphabetical
+
+# np2 = np.array(["John", "Tina", "Aaron", "Zed"])
+# print(np2)
+
+# print(np.sort(np2))
+
+# Booleans T/F
+
+# np3 = np.array([True, False, False, True])
+
+# print(np3)
+# print(np.sort(np3))
+
+
+# Return a copy not change the Original
+
+# print(np1)
+# print(np.sort(np1))
+# print(np1)
+
+
+# 2-D Array
+
+np4 = np.array([[6,7,1,9],[8,3,5,0]])
+print(np4)
+print(np.sort(np4))
