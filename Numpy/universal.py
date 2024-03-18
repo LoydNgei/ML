63a75ea036403417d27872d7a11ee8fb7e1c8d4c
Loyd Ngei <loydngei@gmail.com>
2024-03-18 16:42:18 +0300

    Universal functions in Numpy

---
+++ Numpy/universal.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Numpy Universal Function
+
+np1 = np.array([-5, -4, -3, -2, -1, 0,1,2,3,4,5,6,7,8,9])
+print(np1)
+
+
+# Square Root of each element
+
+print(np.sqrt(np1))
+
+# Absolute Value
+
+print(np.absolute(np1))
+
+# Exponents
+
+print(np.exp(np1))
+
+# Trigonometry (sin, cos, log)
+
+print(np.sin(np1))
+print(np.log(np1))
